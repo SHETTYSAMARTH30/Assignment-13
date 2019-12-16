ef299017f315e652cfbd4ad6d6d6bed321f4981d
SHETTYSAMARTH30 <samarthshetty95@gmail.com>
2019-12-16 01:56:54 -0800

    Module 13: Node.js
    The code for the project is available in the following folders.

---
+++ build/precache-manifest.1da7310d6d016e29d93d6cecf6cf6dc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b759d0d912560b0789fd1d57db7f06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb84d8ba2fe2dcc0fe4b",
+    "url": "/static/css/main.324c2a1d.chunk.css"
+  },
+  {
+    "revision": "23abcbfefca95e9ed58c",
+    "url": "/static/js/2.7beeb27e.chunk.js"
+  },
+  {
+    "revision": "eb84d8ba2fe2dcc0fe4b",
+    "url": "/static/js/main.bef8246c.chunk.js"
+  },
+  {
+    "revision": "d43e78c8bbb21d4f0e7e",
+    "url": "/static/js/runtime-main.c35eacba.js"
+  }
+]);
--- src/Products.js
+++ src/Products.js
@@ -3,48 +3,89 @@ import Filters from './Filters'
 import ProductTable from './ProductTable'
 import ProductForm from './ProductForm'
 
-let PRODUCTS = {
-    '1': {id: 1, category: 'Music', price: '$459.99', name: 'Clarinet'},
-    '2': {id: 2, category: 'Music', price: '$5,000', name: 'Cello'},
-    '3': {id: 3, category: 'Music', price: '$3,500', name: 'Tuba'},
-    '4': {id: 4, category: 'Furniture', price: '$799', name: 'Chaise Lounge'},
-    '5': {id: 5, category: 'Furniture', price: '$1,300', name: 'Dining Table'},
-    '6': {id: 6, category: 'Furniture', price: '$100', name: 'Bean Bag'}
-};
+let PRODUCTS = [];
+
+
 
 class Products extends Component {
     constructor(props) {
         super(props)
         this.state = {
             filterText: '',
-            products: PRODUCTS
+			products: PRODUCTS,
+			currProduct: null
         }
         this.handleFilter = this.handleFilter.bind(this)
+        this.handleEdit = this.handleEdit.bind(this)
         this.handleDestroy = this.handleDestroy.bind(this)
         this.handleSave = this.handleSave.bind(this)
-    }
+	}
+	
+	componentDidMount() { this.getProducts() }
+
+	getProducts() {
+		fetch( "/products/get")
+		.then((res) => res.json())
+		.then((res) => {
+			if (res.success === 0) 
+			return alert(res.message);
+			this.setState({ products: res.data })
+		})
+	}
 
     handleFilter(filterInput) {
         this.setState(filterInput)
     }
 
     handleSave(product) {
-        if (!product.id) {
-            product.id = new Date().getTime()
-        }
-        this.setState((prevState) => {
-            let products = prevState.products
-            products[product.id] = product
-            return { products }
-        })
+		if (product.productid != null) {
+			
+
+			fetch("/products/update/" + product.productid, {
+				body: JSON.stringify(product),
+				method: "POST",
+				headers: {'Content-Type': 'application/json'}
+			})
+			.then(res => res.json())
+			.then((res) => {
+				if(res.success === 0) 
+				return alert(res.message);
+				this.getProducts();
+			})
+			.catch(err => console.error(err))
+		} else {
+			
+
+			fetch("/products/create", {
+				body: JSON.stringify(product),
+				method: "POST",
+				headers: {'Content-Type': 'application/json'}
+			})
+			.then(res => res.json())
+			.then((res) => {
+				if(res.success === 0) 
+				return alert(res.message);
+				this.getProducts();
+			})
+			.catch(err => console.error(err))
+		}
     }
 
+	handleEdit(productId) {
+		let currProduct = this.state.products.find(p => p.id === productId);
+		this.setState({
+			currProduct: currProduct
+		})
+	}
+
     handleDestroy(productId) {
-        this.setState((prevState) => {
-            let products = prevState.products
-            delete products[productId]
-            return { products }
-        });
+		fetch("/products/delete/" + productId)
+		.then((res) => res.json())
+		.then((res) => {
+			if (res.success === 0) 
+			return alert(res.message);
+			this.getProducts();
+		})
     }
 
     render () {
@@ -53,12 +94,16 @@ class Products extends Component {
                 <h1>My Inventory</h1>
                 <Filters 
                     onFilter={this.handleFilter}></Filters>
-                <ProductTable 
+				<ProductTable 
+					filterText={this.state.filterText}
                     products={this.state.products}
-                    filterText={this.state.filterText}
-                    onDestroy={this.handleDestroy}></ProductTable>
+					onDestroy={this.handleDestroy}
+                    onEdit={this.handleEdit}
+                   ></ProductTable>
                 <ProductForm
-                    onSave={this.handleSave}></ProductForm>
+					key={JSON.stringify(this.state.currProduct)}
+					onSave={this.handleSave}
+					product={this.state.currProduct}></ProductForm>
             </div>
         )
     }
